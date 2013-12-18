bd506b69f7405fa3154c633608057d8fd3f22eac
JJMills <joel.j.mills@gmail.com>
2013-12-18 16:07:01 +0000

    Problem 2 completed in Java

---
+++ Java/src/Problem2.java
@@ -0,0 +1,16 @@
+public class Problem2 {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int total = 2; //Starting at 2 because the below excludes the first two values of the sequence.
+		while (b < 4000000) {
+			int c = a + b;
+			a = b;
+			b = c;
+			if (c % 2 == 0) {
+				total += c;
+			}
+		}
+		System.out.println(total);
+	}
+}
