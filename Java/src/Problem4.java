8237d5533a70916a577d9dfec1ef3d8a2b9b0cc1
JJMills <joel.j.mills@gmail.com>
2013-12-18 20:50:11 +0000

    Accidentally committed a test version

--- Java/src/Problem4.java
+++ Java/src/Problem4.java
@@ -1,7 +1,7 @@
 public class Problem4 {
 	public static void main(String[] args) {
 		int palindrome = 0;
-		for (int i = 99; i > 0; i--) {
+		for (int i = 999; i > 0; i--) {
 			for (int j = i; j > 0; j--) { //j = i to avoid repetition.
 				int k = i * j;
 				if (isPalindrome(k) && k > palindrome) {
