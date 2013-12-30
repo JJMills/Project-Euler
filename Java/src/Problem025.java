e990206c883fac9674417490054d70d4839209c9
JJMills <joel.j.mills@gmail.com>
2013-12-30 21:14:23 +0000

    Problem 25 completed

---
+++ Java/src/Problem025.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+public class Problem025 {
+	public static void main(String[] args) {
+		BigInteger a = BigInteger.ONE;
+		BigInteger b = BigInteger.ZERO;
+		BigInteger limit = BigInteger.TEN.pow(999);
+		int count = 0;
+		while (b.compareTo(limit) != 1) {
+			BigInteger temp = b.add(a);
+			a = b;
+			b = temp;
+			count++;
+		}
+		System.out.println(count);
+	}
+}
