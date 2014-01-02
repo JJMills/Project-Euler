0c1cf26fbdaab9cdccba0b47fc582c070f6b297a
JJMills <joel.j.mills@gmail.com>
2014-01-02 00:58:45 +0000

    Optimisation broke the solution, fixed it

--- Python/src/Problem001.py
+++ Python/src/Problem001.py
@@ -2,8 +2,8 @@ __author__ = 'Joel'
 
 
 def multiples():
-    return sum([3 * x for x in range(1, 1000 // 3)]) + sum([5 * x for x in range(1, 1000 // 5)]) - sum(
-        [15 * x for x in range(1, 1000 // 15)])
+    return sum([3 * x for x in range(1, 1000 // 3 + 1)]) + sum([5 * x for x in range(1, 1000 // 5)]) - sum(
+        [15 * x for x in range(1, 1000 // 15 + 1)])
 
 
 print(multiples())
