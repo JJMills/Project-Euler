463081ac7541d1c5e62c6eb1a4336c39710dd909
JJMills <joel.j.mills@gmail.com>
2014-04-30 01:36:57 +0100

    Add unit tests

---
+++ Java/tests/ProblemsUnitTest.java
@@ -0,0 +1,96 @@
+import org.junit.Test;
+import solutions.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProblemsUnitTest {
+    @Test
+    public void testProblem001() {
+        assertEquals(233168, new Problem001().solve());
+    }
+
+    @Test
+    public void testProblem002() {
+        assertEquals(4613732, new Problem002().solve());
+    }
+
+    @Test
+    public void testProblem003() {
+        assertEquals(6857, new Problem003().solve());
+    }
+
+    @Test
+    public void testProblem004() {
+        assertEquals(906609, new Problem004().solve());
+    }
+
+    @Test
+    public void testProblem005() {
+        assertEquals(232792560, new Problem005().solve());
+    }
+
+    @Test
+    public void testProblem006() {
+        assertEquals(25164150, new Problem006().solve());
+    }
+
+    @Test
+    public void testProblem007() {
+        assertEquals(104743, new Problem007().solve());
+    }
+
+    @Test
+    public void testProblem008() {
+        assertEquals(40824, new Problem008().solve());
+    }
+
+    @Test
+    public void testProblem009() {
+        assertEquals(31875000, new Problem009().solve());
+    }
+
+    @Test
+    public void testProblem010() {
+        assertEquals(142913828922L, new Problem010().solve());
+    }
+
+    @Test
+    public void testProblem011() {
+        assertEquals(70600674, new Problem011().solve());
+    }
+
+    @Test
+    public void testProblem013() {
+        assertEquals("5537376230", new Problem013().solve());
+    }
+
+    @Test
+    public void testProblem015() {
+        assertEquals(137846528820L, new Problem015().solve());
+    }
+
+    @Test
+    public void testProblem017() {
+        assertEquals(21124, new Problem017().solve());
+    }
+
+    @Test
+    public void testProblem019() {
+        assertEquals(171, new Problem019().solve());
+    }
+
+    @Test
+    public void testProblem021() {
+        assertEquals(31626, new Problem021().solve());
+    }
+
+    @Test
+    public void testProblem023() {
+        assertEquals(4179871, new Problem023().solve());
+    }
+
+    @Test
+    public void testProblem025() {
+        assertEquals(4782, new Problem025().solve());
+    }
+}
